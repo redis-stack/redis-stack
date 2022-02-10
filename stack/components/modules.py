2641fbd38c562002d8b45d05a376ad88ac54bde4
Chayim I. Kirshen <c@kirshen.com>
2022-02-10 12:38:37 +0200

    adding nodejs to the base package

---
+++ stack/components/modules.py
@@ -0,0 +1,163 @@
+import semantic_version
+from loguru import logger
+import zipfile
+import requests
+import shutil
+import urllib
+import os
+from ..paths import Paths
+
+
+class Modules(object):
+    """Helper class for handling redis modules"""
+
+    AWS_S3_BUCKET = "redismodules.s3.amazonaws.com"
+    MODULE_VERSIONS = {
+        "REJSON": "2.0.6",
+        "REDISGRAPH": "2.8.7",
+        "REDISTIMESERIES": "1.6.7",
+        "REDISEARCH": "2.2.7",
+        "REDISGEARS": "1.2.2",
+        "REDISBLOOM": "2.2.9",
+        "REDISAI": None,
+        "REDISINSIGHT": None,
+    }
+
+    def __init__(self, osnick: str, arch: str = "x86_64", osname: str = "Linux"):
+        self.OSNICK = osnick
+        self.ARCH = arch
+        self.OSNAME = osname
+        self.__PATHS__ = Paths(osnick, arch, osname)
+
+    def generate_url(self, module: str, version: str):
+        """Assuming the module follows the standard, return the URL from
+        which to grab it"""
+        try:
+            semantic_version.Version(version)
+            return urllib.parse.urljoin(
+                f"https://{self.AWS_S3_BUCKET}",
+                f"{module}/{module}.{self.OSNAME}-{self.OSNICK}-{self.ARCH}.{version}.zip",
+            )
+        except ValueError:
+            return urllib.parse.urljoin(
+                f"https://{self.AWS_S3_BUCKET}",
+                f"{module}/snapshots/{module}.{self.OSNAME}-{self.OSNICK}-{self.ARCH}.{version}.zip",
+            )
+
+    def rejson(self, version: str = MODULE_VERSIONS["REJSON"]):
+        """rejson specific fetch"""
+        logger.info("Fetching rejson")
+        destfile = os.path.join(
+            self.__PATHS__.EXTERNAL,
+            f"rejson-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip",
+        )
+        url = self.generate_url("rejson", version)
+        self._fetch_and_unzip(url, destfile)
+        shutil.copyfile(
+            os.path.join(self.__PATHS__.DESTDIR, "rejson.so"),
+            os.path.join(self.__PATHS__.LIBDIR, "rejson.so"),
+        )
+
+    def redisgraph(self, version: str = MODULE_VERSIONS["REDISGRAPH"]):
+        """redisgraph specific fetch"""
+        logger.info("Fetching redisgraph")
+        destfile = os.path.join(
+            self.__PATHS__.EXTERNAL,
+            f"redisgraph-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip",
+        )
+        url = self.generate_url("redisgraph", version)
+        self._fetch_and_unzip(url, destfile)
+        shutil.copyfile(
+            os.path.join(self.__PATHS__.DESTDIR, "redisgraph.so"),
+            os.path.join(self.__PATHS__.LIBDIR, "redisgraph.so"),
+        )
+
+    def redisearch(self, version: str = MODULE_VERSIONS["REDISEARCH"]):
+        """redisearch specific fetch"""
+        logger.info("Fetching redisearch")
+        destfile = os.path.join(
+            self.__PATHS__.EXTERNAL,
+            f"redisearch-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip",
+        )
+        url = self.generate_url("redisearch", version)
+        self._fetch_and_unzip(url, destfile)
+        shutil.copyfile(
+            os.path.join(self.__PATHS__.DESTDIR, "module-enterprise.so"),
+            os.path.join(self.__PATHS__.LIBDIR, "redisearch.so"),
+        )
+
+    def redistimeseries(self, version: str = MODULE_VERSIONS["REDISTIMESERIES"]):
+        """redistimeseries specific fetch"""
+        logger.info("Fetching redistimeseries")
+        destfile = os.path.join(
+            self.__PATHS__.EXTERNAL,
+            f"redistimeseries-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip",
+        )
+        url = self.generate_url("redistimeseries", version)
+        self._fetch_and_unzip(url, destfile)
+        shutil.copyfile(
+            os.path.join(self.__PATHS__.DESTDIR, "redistimeseries.so"),
+            os.path.join(self.__PATHS__.LIBDIR, "redistimeseries.so"),
+        )
+
+    def redisbloom(self, version: str = MODULE_VERSIONS["REDISBLOOM"]):
+        """bloom specific fetch"""
+        logger.info("Fetching redisbloom")
+        destfile = os.path.join(
+            self.__PATHS__.EXTERNAL,
+            f"redisbloom-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip",
+        )
+        url = self.generate_url("redisbloom", version)
+        self._fetch_and_unzip(url, destfile)
+        shutil.copyfile(
+            os.path.join(self.__PATHS__.DESTDIR, "redisbloom.so"),
+            os.path.join(self.__PATHS__.LIBDIR, "redisbloom.so"),
+        )
+
+    # def redisinsight(self, version: str = self.MODULE_VERSIONS["REDISINSIGHT"]):
+    #     """bloom specific fetch"""
+    #     # logger.info("Fetching redisinsight")
+    #     URLS = {
+    #         "macos": "https://download.redisinsight.redis.com/latest/RedisInsight-preview-mac-x64.dmg",
+    #         "windows": "https://download.redisinsight.redis.com/latest/RedisInsight-preview-win-installer.exe",
+    #         "Linux": "https://d3fyopse48vfpi.cloudfront.net/latest/redisinsight-linux64",
+    #     }
+    #     pass
+
+    def _fetch_and_unzip(self, url: str, destfile: str):
+
+        logger.debug(f"Package URL: {url}")
+
+        if os.path.isfile(destfile):
+            return
+
+        r = requests.get(url, stream=True)
+        if r.status_code > 204:
+            logger.error(f"{url} could not be retrieved")
+            raise requests.HTTPError
+        open(destfile, "wb").write(r.content)
+
+        logger.debug(f"Unzipping {destfile} and storing in {self.__PATHS__.DESTDIR}")
+        with zipfile.ZipFile(destfile, "r") as zp:
+            zp.extractall(self.__PATHS__.DESTDIR)
+
+    # FUTURE include in the future, when gears is part of redis stack
+    # def redisgears(self, version: str = MODULE_VERSIONS["REDISGEARS"]):
+    #     """gears specific fetch"""
+    #     logger.info("Fetching redisgears")
+    #     destfile = os.path.join(
+    #         EXTERNAL, f"redisgears-{self.OSNAME}-{self.OSNICK}-{self.ARCH}.zip"
+    #     )
+    #     url = self.generate_url("redisgears", version)
+    #     self._fetch_and_unzip(url, destfile)
+    #     shutil.copyfile(
+    #         os.path.join(self.DESTDIR, "redisgears.so"),
+    #         os.path.join(self.LIBDIR, "redisgears.so"),
+    #     )
+
+    # logger.info("Fetching redisbloom")
+
+    # def redisai(self, version: str = MODULE_VERSIONS["REDISAI"]):
+    #     """bloom specific fetch"""
+    #     # logger.info("Fetching redisai")
+    #     pass
---
+++ stack/package.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+
+from abc import abstractclassmethod
+import inspect
+import os
+import shutil
+import requests
+import jinja2
+import tempfile
+from loguru import logger
+from stack.paths import Paths
+from stack.components.modules import Modules
+from stack.components.nodejs import NodeJS
+
+
+# REDIS STACK PACKAGE RULES
+PRODUCT = "redis-stack"
+VENDOR = "Redis Inc"
+VERSION = "1.0.0"
+EMAIL = "Redis OSS <oss@redislabs.com>"
+LICENSE = "MIT"
+PRODUCT_USER = "redis"
+PRODUCT_GROUP = "redis"
+
+# TODO need to edit these
+SUMMARY = "Some king od siummary, I too am a placeholder"
+DESCRIPTION = "A placeholder for some sort of description"
+
+
+class Package:
+    def __init__(self, osnick, arch="x86_64", osname="Linux"):
+        self.OSNICK = osnick
+        self.ARCH = arch
+        self.OSNAME = osname
+        self.__PATHS__ = Paths(osnick, arch, osname)
+
+    @property
+    def __package_base_args__(self) -> list:
+        """Return base arguments for the package."""
+        return [
+            "fpm",
+            "-s dir",
+            f"-C {self.__PATHS__.WORKDIR}",
+            f"-n {PRODUCT}",
+            "--provides redis",
+            "--provides redis-server",
+            f"--architecture {self.ARCH}",
+            f"--vendor '{VENDOR}'",
+            f"--version {VERSION}",
+            f"--url 'https://redistack.io'",
+            f"--license {LICENSE}",
+            f"--category server",
+            f"--maintainer '{EMAIL}'",
+            f"--description '{DESCRIPTION}'",
+        ]
+
+    def prepackage(
+        self, binary_dir: str, ignore: bool = False, version_override: str = None
+    ):
+        for i in [
+            self.__PATHS__.EXTERNAL,
+            self.__PATHS__.DESTDIR,
+            self.__PATHS__.LIBDIR,
+            self.__PATHS__.BINDIR,
+            self.__PATHS__.USRBINDIR,
+        ]:
+            os.makedirs(i, exist_ok=True, mode=0o755)
+
+        m = Modules(self.OSNICK, self.ARCH, self.OSNAME)
+        for i in [
+            m.redisearch,
+            m.redisgraph,
+            m.redistimeseries,
+            m.rejson,
+            # self.redisbloom,
+            # self.redisinsight,
+            # self.redisgears, self.redisai
+        ]:
+            try:
+                if version_override is None:
+                    i()
+                else:
+                    i(version_override)
+            except requests.HTTPError:
+                if ignore:
+                    pass
+                else:
+                    raise
+
+        logger.debug(f"Copying redis binaries from {binary_dir}")
+        for i in [
+            "redis-benchmark",
+            "redis-check-aof",
+            "redis-check-rdb",
+            "redis-cli",
+            "redis-sentinel",
+            "redis-server",
+        ]:
+            dest = os.path.join(self.__PATHS__.BINDIR, i)
+            shutil.copy2(os.path.join(binary_dir, i), dest)
+            os.chmod(dest, mode=0o755)
+
+            # copy configuration files
+            shutil.copytree(
+                os.path.join(self.__PATHS__.SCRIPTDIR, "conf"),
+                self.__PATHS__.CONFDIR,
+                dirs_exist_ok=True,
+            )
+
+        n = NodeJS(self.OSNICK, self.ARCH, self.OSNAME)
+        n.prepare()
+
+    def package(
+        self,
+        package_type: str = "deb",
+        build_number: int = 1,
+        distribution: str = "bionic",
+    ):
+        logger.info(f"Building {package_type} package")
+        fpmargs = self.__package_base_args__
+        fpmargs.append(f"--iteration {build_number}")
+
+        if package_type == "deb":
+            fpmargs.append(
+                f"-p {PRODUCT}-{VERSION}-{build_number}.{distribution}.{self.ARCH}.deb"
+            )
+            fpmargs.append(f"--deb-user {PRODUCT_USER}")
+            fpmargs.append(f"--deb-group {PRODUCT_GROUP}")
+            fpmargs.append(f"--deb-dist {distribution}")
+            fpmargs.append("-t deb")
+
+            if not os.path.isdir(self.__PATHS__.SVCDIR):
+                os.makedirs(self.__PATHS__.SVCDIR)
+            shutil.copyfile(
+                os.path.join(self.__PATHS__.SCRIPTDIR, "redis-stack.service"),
+                os.path.join(self.__PATHS__.SVCDIR, "redis-stack.service"),
+            )
+            fpmargs.append(
+                f"--config-files {(os.path.join(self.__PATHS__.SVCDIR, 'redis-stack.service'))}"
+            )
+
+        elif package_type == "rpm":
+            fpmargs.append(
+                f"-p {PRODUCT}-{VERSION}-{build_number}.{distribution}.{self.ARCH}.rpm"
+            )
+            fpmargs.append(f"--rpm-user {PRODUCT_USER}")
+            fpmargs.append(f"--rpm-group {PRODUCT_GROUP}")
+            fpmargs.append(f"--rpm-dist {distribution}")
+            fpmargs.append("-t rpm")
+
+            if not os.path.isdir(self.__PATHS__.SVCDIR):
+                os.makedirs(self.__PATHS__.SVCDIR)
+            shutil.copyfile(
+                os.path.join(self.__PATHS__.SCRIPTDIR, "redis-stack.service"),
+                os.path.join(self.__PATHS__.SVCDIR, "redis-stack.service"),
+            )
+            fpmargs.append(
+                f"--config-files {(os.path.join(self.__PATHS__.SVCDIR, 'redis-stack.service'))}"
+            )
+        elif package_type == "osxpkg":
+            fpmargs.append(
+                f"-p {PRODUCT}-{VERSION}-{build_number}.{distribution}.{self.ARCH}.osxpkg"
+            )
+            fpmargs.append("-t osxpkg")
+        elif package_type == "pacman":
+            fpmargs.append(f"-p {PRODUCT}-{VERSION}-{build_number}.{self.ARCH}.pacman")
+            fpmargs.append(f"--pacman-user {PRODUCT_USER}")
+            fpmargs.append(f"--pacman-group {PRODUCT_GROUP}")
+            fpmargs.append("--pacman-compression gz")
+            fpmargs.append("-t pacman")
+        elif package_type == "snap":
+            snap_grade = "stable"
+            snap_confinement = "classic"
+
+            vars = {
+                "PRODUCT": PRODUCT,
+                "VERSION": VERSION,
+                "SUMMARY": SUMMARY,
+                "DESCRIPTION": DESCRIPTION,
+                "SNAP_GRADE": snap_grade,
+                "SNAP_CONFINEMENT": snap_confinement,
+            }
+
+            # generate the snapcraft.yaml from the template
+            dest = os.path.join(self.__PATHS__.HERE, "snapcraft.yaml")
+            dest = tempfile.mktemp(suffix=".yaml", prefix="snapcraft")
+            src = "snapcraft.j2"
+
+            loader = jinja2.FileSystemLoader(self.__PATHS__.HERE)
+            env = jinja2.Environment(loader=loader)
+            tmpl = loader.load(name=src, environment=env)
+            generated = tmpl.render(vars)
+            with open(dest, "w+") as fp:
+                fp.write(generated)
+
+            fpmargs.append(f"-p {PRODUCT}-{VERSION}-{build_number}.{self.ARCH}.snap")
+            fpmargs.append(f"--snap-confinement {snap_confinement}")
+            fpmargs.append(f"--snap-grade {snap_grade}")
+            fpmargs.append(f"--snap-yaml {dest}")
+            fpmargs.append("-t snap")
+
+        else:
+            raise AttributeError(f"{package_type} is an invalid package type")
+
+        cmd = " ".join(fpmargs)
+        logger.debug(f"Packaging: {cmd}")
+        return os.system(cmd)
