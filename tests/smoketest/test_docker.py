7190c2e698edbb33fc818c213c81ca159435317b
Chayim I. Kirshen <c@kirshen.com>
2022-02-14 13:54:31 +0200

    linter

--- tests/smoketest/test_docker.py
+++ tests/smoketest/test_docker.py
@@ -2,11 +2,12 @@ import pytest
 import docker
 from helpers import ServiceTestMixin
 
+
 @pytest.mark.docker
 class TestDocker(ServiceTestMixin):
-    
-    DOCKER_NAME = 'redis-stack'
-    
+
+    DOCKER_NAME = "redis-stack"
+
     @classmethod
     def setup_class(cls):
         cls.env = docker.from_env()
@@ -15,10 +16,10 @@ class TestDocker(ServiceTestMixin):
             name=cls.DOCKER_NAME,
             detach=True,
             publish_all_ports=True,
-            ports={'6379/tcp': 6379, '8081/tcp': 8081}
+            ports={"6379/tcp": 6379, "8081/tcp": 8081},
         )
         container.reload()
-    
+
     def teardown_class(cls):
         container = cls.env.containers.get(cls.DOCKER_NAME)
         container.stop()
