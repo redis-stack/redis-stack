8b7a8081ee9bf9f09be463bf28715a1d36f431ec
Chayim I. Kirshen <c@kirshen.com>
2022-03-20 13:59:15 +0200

    redisinsight version

--- stack/components/redisinsight.py
+++ stack/components/redisinsight.py
@@ -32,7 +32,7 @@ class RedisInsightBase(object):
             arch = "x64"
         else:
             arch = self.ARCH
-        return f"https://s3.amazonaws.com/redisinsight.test/public/rs-ri-builds/{version}/redisstack/RedisInsight-preview-{self.APPTYPE}-{osname}.{arch}.tar.gz"
+        return f"https://s3.amazonaws.com/redisinsight.test/public/rs-ri-builds/{version}/redisstack/RedisInsight-v2-{self.APPTYPE}-{osname}.{arch}.tar.gz"
 
     def _fetch_and_unzip(self, url: str, destfile: str):
         logger.debug(f"Package URL: {url}")
