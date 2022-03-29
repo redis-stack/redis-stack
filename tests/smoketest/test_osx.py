eb5370a76b5c19054b2eb4923a4067c45509c856
Redis User <redis@Rediss-Mac-mini.local>
2022-03-29 13:58:46 +0300

    package validation tests

--- tests/smoketest/test_osx.py
+++ tests/smoketest/test_osx.py
@@ -1,6 +1,7 @@
 import os
 from helpers import RedisPackagingMixin, RedisTestMixin
 import platform
+import subprocess
 
 
 class TestOSX(RedisPackagingMixin, RedisTestMixin, object):
@@ -44,4 +45,33 @@ class TestOSX(RedisPackagingMixin, RedisTestMixin, object):
             raise IOError("Could not remove brew tap")
         
     def test_redisinsight_is_installed(self):
-        assert os.path.exists("/Applications/RedisInsight-v2")
+        assert os.path.exists("/Applications/RedisInsight-v2.app")
+
+    def test_modules_present(self):
+        for i in ['rejson.so', 'redisgraph.so', 'redisearch.so', 'redisbloom.so', 'redistimeseries.so']:
+            assert os.path.exists(f"{self.BASEPATH}/lib/{i}")
+
+    def test_binaries_present(self):
+        for i in ['redis-server', 'redis-stack-server', 'redis-cli', 'redis-benchmark', 'redis-check-rdb', 'redis-sentinel', 'redis-check-aof']:
+            print(i)
+            assert os.path.exists(f"{self.BASEPATH}/bin/{i}")
+
+    def test_config_present(self):
+        assert os.path.exists(f"{self.BASEPATH}/etc/redis-stack.conf")
+
+    def test_binaries_execute(self):
+        binaries = [
+            "redis-server",
+            "redis-cli",
+            "redis-benchmark",
+            "redis-check-rdb",
+            "redis-sentinel",
+            "redis-check-aof",
+        ]
+
+        for b in binaries:
+            r = subprocess.run([f"{self.basepath}/bin/{b}", "-h"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            assert r.returncode in [0, 1]  # no segfault
+
+        out = subprocess.run([f"{self.basepath}/bin/redis-stack-server", "-h"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        assert out.stdout.decode().lower().find("redis-stack-server") != -1
