becbb002fd6689d0b0d90a40b482158e1d312c2a
Chayim I. Kirshen <c@kirshen.com>
2022-05-10 12:59:22 +0300

    test restoration

--- tests/smoketest/test_baremetal.py
+++ tests/smoketest/test_baremetal.py
@@ -7,61 +7,6 @@ import pytest
 from env import VagrantTestEnv
 from mixins import RedisPackagingMixin, RedisTestMixin
 
-# class VagrantBase(RedisTestMixin, object):
-#     """Test the pre-installed package on bare metal"""
-#
-# PACKAGE_NAME = "redis-stack-server"
-# HOST_TYPE = "vagrant"
-
-
-# def _assert_path_exists(self, path):
-#     cmd = ["vagrant", "ssh", "-c", f"ls -l {path}"]
-#     res = subprocess.run(cmd, cwd=self.workdir)
-#     assert res.returncode == 0
-
-# def test_config_present(self):
-#     # self._assert_path_exists("/opt/redis-stack/etc/redis-stack.conf")
-
-# def test_modules_present(self):
-#     for i in [
-#         "rejson.so",
-#         "redisgraph.so",
-#         "redisearch.so",
-#         "redisbloom.so",
-#         "redistimeseries.so",
-#     ]:
-#         self._assert_path_exists(f"/opt/redis-stack/lib/{i}")
-
-# def test_binaries_present(self):
-#     for i in [
-#         "redis-server",
-#         "redis-stack-server",
-#         "redis-cli",
-#         "redis-benchmark",
-#         "redis-check-rdb",
-#         "redis-sentinel",
-#         "redis-check-aof",
-#     ]:
-#         self._assert_path_exists(f"/opt/redis-stack/bin/{i}")
-
-# def test_binaries_execute(self):
-#     binaries = [
-#         "redis-server",
-#         "redis-cli",
-#         "redis-benchmark",
-#         "redis-check-rdb",
-#         "redis-sentinel",
-#         "redis-check-aof",
-#     ]
-
-#     for b in binaries:
-#         r = subprocess.run(
-#             ["vagrant", "ssh", "-c", f"/opt/redis-stack/bin/{b} -h"],
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#         )
-#         assert r.returncode in [0, 1]  # no segfault
-
 
 class VagrantBase(VagrantTestEnv, RedisPackagingMixin, RedisTestMixin):
     pass
@@ -127,14 +72,14 @@ class TestFocal(DebVagrant):
 
 @pytest.mark.bionic
 @pytest.mark.physical
-class TestBionic(VagrantBase):
+class TestBionic(DebVagrant):
 
     OSNICK = "bionic"
 
 
 @pytest.mark.xenial
 @pytest.mark.physical
-class TestXenial(VagrantBase):
+class TestXenial(DebVagrant):
 
     OSNICK = "xenial"
 
