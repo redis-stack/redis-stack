0d5efcaaa3cc7d87732f3b4100959e08d0e11e31
Chayim I. Kirshen <c@kirshen.com>
2022-05-22 15:23:45 +0300

    upstream

--- tests/smoketest/test_docker_compose.py
+++ tests/smoketest/test_docker_compose.py
@@ -54,7 +54,7 @@ version: "3.9"
 services:
   redis:
     container_name: {self.DOCKER_IMAGE}-dockercompose
-    image: "redis/{self.DOCKER_IMAGE}:{self.VERSION}"
+    image: "redisfab/{self.DOCKER_IMAGE}:{self.VERSION}"
     ports:
       - 6379:6379
 """
@@ -112,7 +112,7 @@ version: "3.9"
 services:
   redis:
     container_name: {self.DOCKER_IMAGE}-dockercompose
-    image: "redis/{self.DOCKER_IMAGE}:{self.VERSION}"
+    image: "redisfab/{self.DOCKER_IMAGE}:{self.VERSION}"
     ports:
       - 6379:6379
       - 8001:8001
