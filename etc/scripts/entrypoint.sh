c2a16989be0ab12e11e772a8ca7a969f4ef9196e
Chayim <chayim@users.noreply.github.com>
2022-06-29 10:49:20 +0300

    Cherry picks from master for 7.0.x latest (#148)
    Co-authored-by: OfirMos <82366525+OfirMos@users.noreply.github.com>

--- etc/scripts/entrypoint.sh
+++ etc/scripts/entrypoint.sh
@@ -13,7 +13,10 @@ if [ -z ${REDIS_DATA_DIR} ]; then
     REDIS_DATA_DIR=/data
 fi
 
-${BASEDIR}/nodejs/bin/node -r ${BASEDIR}/share/redisinsight/api/node_modules/dotenv/config share/redisinsight/api/dist/src/main.js dotenv_config_path=${BASEDIR}/share/redisinsight/.env &
+# when running in redis-stack (as opposed to redis-stack-server)
+if [ -f ${BASEDIR}/nodejs/bin/node ]; then
+    ${BASEDIR}/nodejs/bin/node -r ${BASEDIR}/share/redisinsight/api/node_modules/dotenv/config share/redisinsight/api/dist/src/main.js dotenv_config_path=${BASEDIR}/share/redisinsight/.env &
+fi
 
 if [ -z ${REDISEARCH_ARGS} ]; then
 REDISEARCH_ARGS="MAXSEARCHRESULTS 10000 MAXAGGREGATERESULTS 10000"
@@ -24,7 +27,7 @@ REDISGRAPH_ARGS="MAX_QUEUED_QUERIES 25 TIMEOUT 1000 RESULTSET_SIZE 10000"
 fi
 
 ${CMD} \
-${CONFFILE} -- \
+${CONFFILE} \
 --dir ${REDIS_DATA_DIR} \
 --protected-mode no \
 --daemonize no \
