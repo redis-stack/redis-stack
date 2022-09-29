d5ab0ad3b16bab8feb3eaef43806f33227a9d746
Chayim <chayim@users.noreply.github.com>
2022-09-29 13:25:32 +0300

    [6.2] merging master (#214)

---
+++ tests/smoketest/test_archlinux.py
@@ -0,0 +1,21 @@
+import pytest
+
+from env import DockerTestEnv
+from mixins import RedisPackagingMixin, RedisTestMixin
+
+
+@pytest.mark.archlinux
+class TestArchPackage(DockerTestEnv, RedisTestMixin, RedisPackagingMixin, object):
+
+    PLATFORM = "linux/amd64"
+    DOCKER_NAME = "archlinux:base-devel"
+    CONTAINER_NAME = "redis-stack-archlinux"
+
+    def install(self, container):
+
+        res, out = container.exec_run("pacman -Fy")
+        assert res == 0
+
+        res, out = container.exec_run("pacman -U --noconfirm /build/redis-stack/redis-stack-server.pkg")
+        if res != 0:
+            raise IOError(out)
