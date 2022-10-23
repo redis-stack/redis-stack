84b3de4cbeb284086931a0c4c4a17b6187153196
Chayim <chayim@users.noreply.github.com>
2022-10-23 14:00:01 +0300

    Latest cherry picks from master (#241)

--- tests/smoketest/test_rpms.py
+++ tests/smoketest/test_rpms.py
@@ -41,3 +41,30 @@ class TestRHEL8(RPMTestBase):
     DOCKER_NAME = "oraclelinux:8"
     CONTAINER_NAME = "redis-stack-centos8"
     PLATFORM = "linux/amd64"
+
+@pytest.mark.amazonlinux2
+class TestAmazonLinux2(RPMTestBase):
+    DOCKER_NAME = "amazonlinux:2"
+    CONTAINER_NAME = "redis-stack-aml2"
+    PLATFORM = "linux/amd64"
+
+    def install(self, container):
+
+        res, out = container.exec_run("amazon-linux-extras install epel -y")
+        assert res == 0
+
+        res, out = container.exec_run("iamnotarealcommand")
+        assert res != 0
+
+        # now, install our package
+        res, out = container.exec_run(
+            "yum install -y /build/redis-stack/redis-stack-server.rpm"
+        )
+        if res != 0:
+            raise IOError(out)
+
+    def uninstall(self, container):
+        res, out = container.exec_run("yum remove -y redis-stack-server")
+        if res != 0:
+            raise IOError(out)
+
