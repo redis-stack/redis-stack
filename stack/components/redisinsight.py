a6fd61e8efa37887198bc9d8478929a710ce5167
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 08:01:01 +0000

    Adding tarfile member sanitization to extractall()

--- stack/components/redisinsight.py
+++ stack/components/redisinsight.py
@@ -62,7 +62,26 @@ class RedisInsightBase(object):
         #     zp.extractall(path=os.path.join(self.__PATHS__.DESTDIR, "redisinsight"))
         logger.debug(f"Untarring {destfile} and storing in {self.__PATHS__.DESTDIR}")
         with tarfile.open(destfile) as f:
-            f.extractall(path=os.path.join(self.__PATHS__.DESTDIR, "redisinsight"))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(f, path=os.path.join(self.__PATHS__.DESTDIR,"redisinsight"))
 
     def prepare(self, version: Union[str, None] = None):
         if version is None:
