0d476c80492b6730cd82ca0354c4716537cc7428
vladvildanov <divinez122@outlook.com>
2024-07-02 15:21:48 +0300

    Show output of commands in docker

--- tests/smoketest/test_rpms.py
+++ tests/smoketest/test_rpms.py
@@ -50,12 +50,15 @@ class TestRHEL7(RPMTestBase):
 
     def install(self, container):
         res, out = container.exec_run("sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*")
+        print(out)
         assert res == 0
 
         res, out = container.exec_run("sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*")
+        print(out)
         assert res == 0
 
         res, out = container.exec_run("yum install -y epel-release tar wget")
+        print(out)
         assert res == 0
 
         # validate we properly get bad outputs as bad
